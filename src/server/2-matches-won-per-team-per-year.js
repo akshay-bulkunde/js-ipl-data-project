d69935520a55d33d736b82a9c997fbb339649bea
akshay-bulkunde <akshay.bulkunde.29.3@mountblue.tech>
2024-09-25 12:03:22 +0530

    add output file for Number of matches won per team per year in IPL.

--- src/server/2-matches-won-per-team-per-year.js
+++ src/server/2-matches-won-per-team-per-year.js
@@ -1,34 +1,42 @@
 // Number of matches won per team per year in IPL.
-
-const matches = require("../csvToJson/matches.json");
+const fs = require('fs');
+const matches = require("../../csvToJson/matches.json");
 
 function matchesWonPerTeam(matches) {
   let matchesWonPerYear = {};
-//   let teamObj = {};
+
 
   for (let match in matches) {
-    
+
     let season = matches[match].season;
-    if(season === undefined){
-        return matchesWonPerYear;
+    if (season === undefined) {
+      return matchesWonPerYear;
     }
-    // console.log(season);
-    // break;
+    
     let winner = matches[match].winner;
     if (!matchesWonPerYear.hasOwnProperty(season)) {
-        matchesWonPerYear[season] = {};
+      matchesWonPerYear[season] = {};
     }
-    if(!matchesWonPerYear[season].hasOwnProperty(winner)){
-       
-        matchesWonPerYear[season][winner] = 1;
+    if (!matchesWonPerYear[season].hasOwnProperty(winner)) {
+
+      matchesWonPerYear[season][winner] = 1;
     }
-     else{
-        matchesWonPerYear[season][winner] = matchesWonPerYear[season][winner] + 1;
-     }
-     
-    
+    else {
+      matchesWonPerYear[season][winner] = matchesWonPerYear[season][winner] + 1;
+    }
+
+
   }
   return matchesWonPerYear;
 }
 
-console.log(matchesWonPerTeam(matches));
+let matchesWonPerYearPerTeam = matchesWonPerTeam(matches);
+try {
+  fs.writeFileSync('../public/output/matchesWonPerYearPerTeam.json', JSON.stringify(matchesWonPerYearPerTeam, null, 2));
+  console.log("File parsed successfully");
+}
+catch (error) {
+  console.log("File parsing failed ", error);
+}
+
+module.exports = { matchesWonPerTeam }
