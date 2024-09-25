92972b7dd05162ad0335fb7e8819767476c25322
akshay-bulkunde <akshay.bulkunde.29.3@mountblue.tech>
2024-09-25 12:10:23 +0530

    add values for Extra runs conceded per team in the year 2016

--- src/server/3-extra-runs-per-team.js
+++ src/server/3-extra-runs-per-team.js
@@ -1,16 +1,14 @@
 // Extra runs conceded per team in the year 2016
-const matches = require("../csvToJson/matches.json");
-const deliveries = require("../csvToJson/deliveries.json");
+const fs = require('fs');
+const matches = require("../../csvToJson/matches.json");
+const deliveries = require("../../csvToJson/deliveries.json");
 
-function get2016matchesId(matches) {
+function getmatchesId(matches) {
   let matchID = [];
   for (let match in matches) {
     let season = matches[match]["season"];
     let id = matches[match]["id"];
-    // console.log(id);
-    // break;
-    // console.log(year);
-    // break;
+
     if (season === 2016) {
       matchID.push(id);
     }
@@ -18,30 +16,35 @@ function get2016matchesId(matches) {
   return matchID;
 }
 
-let matchId = get2016matchesId(matches);
-// console.log(matchId);
+let matchId = getmatchesId(matches);
 
 function extraRunsPerTeam(deliveries, matchId) {
-  // console.log(matchId[0]);
-  // break;
-  //let id = matchId[i];
+
   let extraRunConceded = {};
   for (let delivery in deliveries) {
     if (matchId.includes(deliveries[delivery]["match_id"])) {
       let bowlingTeam = deliveries[delivery]["bowling_team"];
       let extraRuns = deliveries[delivery]["extra_runs"];
-    //   console.log(bowlingTeam);
-    //   console.log(extraRuns);
-    //   break;
-        if(!extraRunConceded.hasOwnProperty(bowlingTeam )){
-            extraRunConceded[bowlingTeam] = extraRuns;
-        }
-        else{
-            extraRunConceded[bowlingTeam ] += extraRuns;
-        }
+
+      if (!extraRunConceded.hasOwnProperty(bowlingTeam)) {
+        extraRunConceded[bowlingTeam] = extraRuns;
+      }
+      else {
+        extraRunConceded[bowlingTeam] += extraRuns;
+      }
     }
   }
   return extraRunConceded;
 }
 
-console.log(extraRunsPerTeam(deliveries, matchId));
+let extraRunConceded = extraRunsPerTeam(deliveries, matchId);
+
+try {
+  fs.writeFileSync('../public/output/extraRunsPerTeam.json', JSON.stringify(extraRunConceded, null, 2));
+  console.log("File parsed successfully");
+}
+catch (error) {
+  console.log("File parsing failed ", error);
+}
+
+module.exports = { extraRunsPerTeam };
